1dfa8eed41dda846c042d98da913ff73df85aa86
BarbodH <habibi.k.barbod@gmail.com>
2026-03-25 16:46:14 -0400

    [api]: Init Spring API with new configs

---
+++ api/src/main/kotlin/dev/bitstachio/interdimensional_bazaar/InterdimensionalBazaarApplication.kt
@@ -0,0 +1,11 @@
+package dev.bitstachio.interdimensional_bazaar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InterdimensionalBazaarApplication
+
+fun main(args: Array<String>) {
+	runApplication<InterdimensionalBazaarApplication>(*args)
+}
---
+++ api/src/test/kotlin/dev/bitstachio/interdimensional_bazaar/InterdimensionalBazaarApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.bitstachio.interdimensional_bazaar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InterdimensionalBazaarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
